2bc6ac42d42ed266fc20dcc693cf7625213d500c
TXien <skyholy6@yahoo.com.tw>
2019-05-17 16:33:32 +0800

    Update account.js

--- src/account.js
+++ src/account.js
@@ -1,6 +1,12 @@
+//secp
 const sha256 = require("sha256")
 const { createECDH, ECDH , createHmac } = require('crypto');
 const ecdh = createECDH('secp256k1');
+//eddsa
+var EdDSA = require('elliptic').eddsa;
+var utils = require('elliptic').utils
+var ec = new EdDSA('ed25519');
+
 class NewAccount {
 	constructor(key) {
 		this.PrivateKey = key
@@ -17,5 +23,20 @@ class NewAccount {
 	}
 }
 
-module.exports = {NewAccount};
+class NewAccountEddsa {
+	constructor(key) {
+		this.EC = ec.keyFromSecret(key)
+	}
+	GeneratePrivateKey(secret){
+		return utils.toHex(this.EC.privBytes());
+	}
+	get Address(){
+		return sha256(this.PublicKey).substr(24,64)
+	}
+	get PublicKey(){
+		return sha256(utils.toHex(this.EC.pubBytes()));
+	}
+}
+
+module.exports = {NewAccount, NewAccountEddsa};
 
